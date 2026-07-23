6fb2097753b6ef19cf39530ff56ac331b95eb989
George Florea <george.florea@and-solution.com>
2026-07-23 14:30:41 +0200

    style: :art: run prettier

--- tools/build.ts
+++ tools/build.ts
@@ -10,22 +10,22 @@ const allCodes: EPSG[] = JSON.parse(await readFile(join(import.meta.dirname, '..
 await mkdir(join(OUT_DIR, 'codes'), { recursive: true });
 
 await writeFile(
-    join(OUT_DIR, 'all.js'),
-    'export default ' + JSON.stringify(Object.fromEntries(allCodes.map(e => [e.code, e]))),
-    'utf8'
+  join(OUT_DIR, 'all.js'),
+  'export default ' + JSON.stringify(Object.fromEntries(allCodes.map((e) => [e.code, e]))),
+  'utf8',
 );
 console.log('Wrote all.js');
 
 const promises = allCodes
-    .map(e => {
-        const fileName = join(OUT_DIR, 'codes', `${e.code}.js`);
+  .map((e) => {
+    const fileName = join(OUT_DIR, 'codes', `${e.code}.js`);
 
-        return [
-            cp(join(import.meta.dirname!, '..', 'single.d.ts'), join(OUT_DIR, 'codes', `${e.code}.d.ts`)),
-            writeFile(fileName, 'export default ' + JSON.stringify(e), 'utf8')
-        ];
-    })
-    .flat();
+    return [
+      cp(join(import.meta.dirname!, '..', 'single.d.ts'), join(OUT_DIR, 'codes', `${e.code}.d.ts`)),
+      writeFile(fileName, 'export default ' + JSON.stringify(e), 'utf8'),
+    ];
+  })
+  .flat();
 
 await Promise.all(promises);
 console.log('Wrote all codes to codes/');
